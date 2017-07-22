608545a4ed3ddeee33582de87e7bbb1a0c489d40
Kazuki_Kamizuru <k.amago0721@gmail.com>
2017-07-22 22:38:53 +0900

    fixes #296 タブ→スペースに

--- src/Model/Behavior/SaveBehavior.php
+++ src/Model/Behavior/SaveBehavior.php
@@ -14,34 +14,34 @@ use Cake\ORM\Behavior;
 class SaveBehavior extends Behavior
 {
     /**
-	 * 保存前処理
+     * 保存前処理
      * エンティティに管理項目を設定します。
      *
      * @param Event $event
      * @param EntityInterface $entity
      */
-	public function beforeSave(Event $event, EntityInterface $entity)
+    public function beforeSave(Event $event, EntityInterface $entity)
     {
-		// 新規登録時は登録日時を設定
-		$nowDate = Time::now();
-		$userId = $this->__getLoginUserId();
-		if ($entity->isNew()) {
-			$entity->created = $nowDate;
-			$entity->created_by = $userId;
-		}
-		$entity->modified = $nowDate;
-		$entity->modified_by = $userId;
-	}
+        // 新規登録時は登録日時を設定
+        $nowDate = Time::now();
+        $userId = $this->__getLoginUserId();
+        if ($entity->isNew()) {
+            $entity->created = $nowDate;
+            $entity->created_by = $userId;
+        }
+        $entity->modified = $nowDate;
+        $entity->modified_by = $userId;
+    }
 
     /**
      * ログインユーザIDを取得する。
      *
      * @return string ログインユーザID
      */
-	private function __getLoginUserId() : string
+    private function __getLoginUserId() : string
     {
-	    $session = new Session();
+        $session = new Session();
         $userId = $session->read('Auth.User.userId');
         return !$userId ? str_replace('/', '', ROOT) : $userId;
-	}
+    }
 }
