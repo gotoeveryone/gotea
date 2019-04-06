f2fb6859bc5b268db9613d90e2ab985ffa6ae313
gotoeveryone <k.amago0721@gmail.com>
2019-04-06 14:58:37 +0900

    fixes #648 年フォーマットを yyyy に

--- src/View/Helper/DateHelper.php
+++ src/View/Helper/DateHelper.php
@@ -12,9 +12,9 @@ use Cake\View\Helper;
 class DateHelper extends Helper
 {
     // 日付フォーマット
-    const FORMAT_DATE = 'YYYY年MM月dd日';
+    const FORMAT_DATE = 'yyyy年MM月dd日';
     const FORMAT_TIME = 'HH時mm分ss秒';
-    const FORMAT_DATETIME = 'YYYY年MM月dd日 HH時mm分ss秒';
+    const FORMAT_DATETIME = self::FORMAT_DATE . ' ' . self::FORMAT_TIME;
 
     /**
      * 日付型オブジェクトを特定フォーマットの表記に変換して表示します。
@@ -34,7 +34,7 @@ class DateHelper extends Helper
 
     /**
      * 日付型オブジェクトを特定フォーマットの表記に変換して表示します。
-     * ※YYYY年MM月dd日 HH時mm分ss秒
+     * ※yyyy年MM月dd日 HH時mm分ss秒
      *
      * @param \Cake\I18n\FrozenTime|null $timeObj 変換するTime型オブジェクト
      * @return string 整形した日付表記
@@ -46,7 +46,7 @@ class DateHelper extends Helper
 
     /**
      * 日付型オブジェクトを特定フォーマットの表記に変換して表示します。
-     * ※YYYY年MM月dd日
+     * ※yyyy年MM月dd日
      *
      * @param \Cake\I18n\FrozenDate|null $dateObj 変換するDate型オブジェクト
      * @return string 整形した日付表記
