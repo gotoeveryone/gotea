d7978510e86a28354b1e99a7c014c7ea4534bd40
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-03-11 17:12:59 +0900

    fixes #1085 出力時に存在しないフィールドは処理しない

--- src/Collection/Iterator/RankingIterator.php
+++ src/Collection/Iterator/RankingIterator.php
@@ -17,16 +17,23 @@ class RankingIterator
      */
     public function __invoke(array $item, int $index): array
     {
-        return [
-            'id' => $item['id'],
+        $res = [
             'rank' => $item['win_rank'],
             'name' => $item['name'],
             'win' => $item['win'],
             'lose' => $item['lose'],
             'draw' => $item['draw'],
             'percentage' => $item['percentage'],
-            'sex' => $item['sex'],
-            'url' => $item['url'],
         ];
+
+        // 以下は項目が存在する場合のみ出力
+        $optionFields = ['id', 'sex', 'url'];
+        foreach ($optionFields as $field) {
+            if (isset($item[$field])) {
+                $res[$field] = $item[$field];
+            }
+        }
+
+        return $res;
     }
 }
